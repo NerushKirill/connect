3f33043dc326705f9b76a22c137fe8ef4e73e088
Kirill Nerush <nerush.kirill@mail.ru>
2024-01-22 11:07:44 +0300

    first commit

---
+++ run.sh
@@ -0,0 +1,24 @@
+sudo apt update
+sudo apt install -y openssh-server mc htop net-tools netcat nano git make
+
+sudo cp -r /home/$USER/connect/.ssh /home/$USER/
+
+sudo chmod 700 /home/$USER/.ssh
+sudo chmod 600 /home/$USER/.ssh/authorized_keys
+
+# Install docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
